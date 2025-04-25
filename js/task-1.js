951da9b7aa4697da803f92bee0c3cc75813e3d0a
Yulia Bezsmertna <yuliabezsmertna80@gmail.com>
2025-04-25 18:53:14 +0300

    hm4

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,26 @@
-function slugify(title) {
-const lowerCased = title.toLowerCase();
-const words = lowerCased.split(" ");
-const slug = words.join("-");
-return slug;
-}
+function isEnoughCapacity(products, containerSize) {
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+  const values = Object.values(products);
+
+    let totalProduct = 0;
+    
+    for (const number of values) {
+        totalProduct += number;
+    }
+    return totalProduct <= containerSize;
+}
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  ); // false
